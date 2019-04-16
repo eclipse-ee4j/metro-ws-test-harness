e858986c279e109be0034783cccaa93c05bd0064
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-04-16 17:19:39 +0200

    remove unused imports
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- ws-test-harness/core/src/main/java/com/sun/xml/ws/test/util/FreeMarkerTemplate.java
+++ ws-test-harness/core/src/main/java/com/sun/xml/ws/test/util/FreeMarkerTemplate.java
@@ -14,18 +14,13 @@ package com.sun.xml.ws.test.util;
 import com.sun.xml.ws.test.SourcesCollector;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FreeMarkerTemplate {
 
